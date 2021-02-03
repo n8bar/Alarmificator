54feb1a66aecc50249655de0e58745a64f07eade
Nate Barlow <n8barlow@gmail.com>
2021-02-02 17:54:24 -0700

    Main Layout

---
+++ app/src/main/java/com/natetheprogrammer/alarmificator/App.java
@@ -0,0 +1,14 @@
+package com.natetheprogrammer.alarmificator;
+
+import android.app.Application;
+
+public class App extends Application {
+    public static final String CHANNEL_ID = "Stupopla_ALERT_CHANNEL";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        
+        
+    }
+}
--- app/src/main/java/com/natetheprogrammer/alarmificator/MainActivity.java
+++ app/src/main/java/com/natetheprogrammer/alarmificator/MainActivity.java
@@ -3,12 +3,19 @@ package com.natetheprogrammer.alarmificator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+    }
+    
+    private void scheduleAlarm(View view) {
+        
     }
 }
