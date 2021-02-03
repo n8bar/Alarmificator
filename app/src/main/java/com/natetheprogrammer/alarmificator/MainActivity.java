d09352ffbd6a45ee94644e9cf4434bfc852abc6a
Nate Barlow <n8barlow@gmail.com>
2021-02-02 22:02:08 -0700

    Instantiate an AlarmManager

--- app/src/main/java/com/natetheprogrammer/alarmificator/MainActivity.java
+++ app/src/main/java/com/natetheprogrammer/alarmificator/MainActivity.java
@@ -2,6 +2,10 @@ package com.natetheprogrammer.alarmificator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,6 +20,17 @@ public class MainActivity extends AppCompatActivity {
     }
     
     private void scheduleAlarm(View view) {
+        Context context=view.getContext();
+        int alarmId=0;
         
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+
+        //Intent intent = new Intent(context, AlarmBroadcastReceiver.class);
+        //intent.putExtra("Title", "Alarm Title");
+        //intent.putExtra("Message", "Yo!");
+
+        //PendingIntent pi = PendingIntent.getBroadcast(context, alarmId, intent, 0 );
+        
+        //alarmManager.setExact(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), pi);
     }
 }
